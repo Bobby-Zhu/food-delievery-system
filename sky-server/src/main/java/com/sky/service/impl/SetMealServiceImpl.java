fb790ee69fd41e99504c79759108f137a375b37c
Bobby-Zhu <804962881z@gmail.com>
2025-07-15 23:30:10 -0400

    user end controller with frontend

--- sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
+++ sky-server/src/main/java/com/sky/service/impl/SetMealServiceImpl.java
@@ -13,6 +13,7 @@ import com.sky.mapper.SetMealDishMapper;
 import com.sky.mapper.SetMealMapper;
 import com.sky.result.PageResult;
 import com.sky.service.SetMealService;
+import com.sky.vo.DishItemVO;
 import com.sky.vo.DishVO;
 import com.sky.vo.SetmealVO;
 import org.springframework.beans.BeanUtils;
@@ -113,5 +114,24 @@ public class SetMealServiceImpl implements SetMealService {
                             .id(setmealId)
                             .build();
         setmealMapper.update(setmeal);
+    }
+
+    /**
+     * 条件查询
+     * @param setmeal
+     * @return
+     */
+    public List<Setmeal> list(Setmeal setmeal) {
+        List<Setmeal> list = setmealMapper.list(setmeal);
+        return list;
+    }
+
+    /**
+     * 根据id查询菜品选项
+     * @param id
+     * @return
+     */
+    public List<DishItemVO> getDishItemById(Long id) {
+        return setmealMapper.getDishItemBySetmealId(id);
     }
 }
